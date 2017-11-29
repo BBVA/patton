c4d7d95bdf2e9b7e569d9d8bbecc4fbf3f1c35c6
iknite <enrique@iknite.com>
2017-11-29 13:35:23 +0100

    Add dumb test

--- tests/test_patton.py
+++ tests/test_patton.py
@@ -6,13 +6,5 @@ Tests for `patton` module.
 """
 
 
-import pytest
-
-
-@pytest.fixture
-def response():
-    """
-    Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    return 'response'
+def test_dumb():
+    assert True
