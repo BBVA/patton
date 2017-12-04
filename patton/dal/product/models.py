566460edb89c2e4aa55e2036cc3ca47c98e169da
iknite <enrique@iknite.com>
2017-12-04 14:31:15 +0100

    Normalize database field names

--- patton/dal/product/models.py
+++ patton/dal/product/models.py
@@ -7,7 +7,7 @@ from . import fields
 
 class Prod(Base):
     __tablename__ = 'prod'
-    cpe22 = Column(String, primary_key=True)
+    id = Column(String, primary_key=True)
     title = Column(String)
     title_lang = Column(String)
 
@@ -18,7 +18,7 @@ class Prod(Base):
     def loader_map(root):
         return [
             {
-                'cpe22': cpe.attrib['name'],
+                'id': cpe.attrib['name'],
                 'title': cpe.find('{*}title').text,
                 'title_lang': cpe.find('{*}title').attrib['{http://www.w3.org/XML/1998/namespace}lang']
             }
@@ -29,7 +29,7 @@ class Prod(Base):
 class ProdReference(Base):
     __tablename__ = 'prod_reference'
     id = Column(String, primary_key=True, default=fields.uuid)
-    prod = Column(String, ForeignKey('prod.cpe22'))
+    prod_id = Column(String, ForeignKey('prod.id'))
     href = Column(String)
     description = Column(String)
 
@@ -47,14 +47,14 @@ class ProdReference(Base):
 
 class Cpe23(Base):
     __tablename__ = 'cpe23'
-    name = Column('name', String, primary_key=True)
-    prod = Column('cpe', String, ForeignKey('prod.cpe22'))
+    id = Column(String, primary_key=True)
+    prod_id = Column(String, ForeignKey('prod.id'))
 
     def loader_map(root):
         return [
             {
-                'name': cpe23.attrib['name'],
-                'cpe': cpe23.getparent().attrib['name'],
+                'id': cpe23.attrib['name'],
+                'prod': cpe23.getparent().attrib['name'],
             }
             for cpe23 in root.iter('{*}cpe23-item')
         ]
