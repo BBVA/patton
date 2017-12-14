579b431daafd310c86af49be86c62232911b0586
iknite <enrique@iknite.com>
2017-12-14 09:54:01 +0100

    Add app wrappers for dal.managers

--- patton/app.py
+++ patton/app.py
@@ -5,20 +5,25 @@ from sanic.exceptions import NotFound
 from . import config
 from . import __version__
 
+from .dal.managers import batch_query_cpe, query_cpe
+
 app = Sanic(__name__)
 app.config.from_object(config)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 async def hello_world(*args, **kwargs):
     return text(f'Welcome to patton! {__version__}')
 
 
-@app.route('/pkg/<name>/<version>')
-async def package_search(request, name, version, *args, **kwargs):
-    with session_ctx() as session:
-        query = session.query(VulnProduct).first()
-    return json(query)
+@app.route('/<package>/<version>', methods=['GET'])
+def package_search(request, package, version, *args, **kwargs):
+    return json(query_cpe(package, version))
+
+
+@app.route('/batch/', methods=['POST'])
+def batch_package_search(request, *args, **kwargs):
+    return json(batch_query_cpe(request.json))
 
 
 @app.exception(NotFound)
